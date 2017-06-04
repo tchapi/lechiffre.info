e3cc0b88f0242f205ac183a43f51557da21a1b20
tchapi <regbasket+github@gmail.com>
2017-06-04 20:28:54 +0200

    Initial commit

---
+++ livreOr.php
@@ -0,0 +1,91 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:og="http://ogp.me/ns#"
+      xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Language" content="fr">
+<meta name="Description" content="Site sur l'histoire du Chiffre à travers les ages : des balbutiements des mathématiques à son utilisation moderne, un voyage passionnant au coeur du symbole mathématique par excellence ...">
+<meta name="Keywords" content="chiffre,nombre,mathématique,maths,math,mathematiques,nombres,chiffres,histoire,mayas,codes,déchiffrage,dechiffrage">
+
+<title>L'Histoire des Nombres : Le Chiffre | Livre d'Or</title>
+<meta property="og:title" content="L'Histoire des Nombres : Le Chiffre | Livre d'Or"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="http://lechiffre.free.fr"/>
+<meta property="og:site_name" content="Le Chiffre"/>
+<meta property="og:description"
+    content="Site sur l'histoire du Chiffre à travers les ages : des balbutiements des mathématiques à son utilisation moderne, un voyage passionnant au coeur du symbole mathématique par excellence ..."/>
+
+<LINK rel="stylesheet" type="text/css" href="style.css">
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12176511-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+   
+</head>
+
+
+<div id="content">
+  <div id="banniere">
+    <img SRC="http://lechiffre.free.fr/banniere.jpg" ALT="L'Histoire des nombres : le chiffre" BORDER=0 height=143 width=587>
+  </div>
+  <div id="titre">
+    Livre d'Or
+  </div>
+    <div id="menu-left">
+	<ul>
+      <li><a href="page_som.html"><img SRC="menu/som-o.png" ALT="Sommaire" BORDER=0 height=29 width=150></a></li>
+   	 <li><a href="page_themes.html"><img SRC="menu/the-o.png" ALT="Thèmes" BORDER=0 height=29 width=150></a></li>
+	 <li><a href="page_auteurs.html"><img SRC="menu/aut-o.png" ALT="Auteurs" BORDER=0 height=29 width=150></a></li>
+	 <li><a href="page_index.html"><img SRC="menu/ind-o.png" ALT="Index" BORDER=0 height=29 width=150></a></li>
+	 <li><a href="page_sources.html"><img SRC="menu/sou-o.png" ALT="Sources" BORDER=0 height=29 width=150></a></li>
+    </ul>
+  </div>
+  
+<div id="contenu">
+
+
+		<table CELLSPACING=0 CELLPADDING=0 COLS=2 WIDTH="700" >
+<tr>
+
+<td ALIGN=LEFT VALIGN=TOP WIDTH="528">
+<center><font face="Arial,Helvetica"><font size=+1>Signez le livre d'or
+en remplissant le formulaire ci-dessous :</font></font>
+<p><form METHOD="POST" ACTION="http://perso0.free.fr/cgi-bin/addguest.pl" onsubmit="return jcap();">
+<p><font face="Arial,Helvetica"><font size=+1>&nbsp;Nom :</font></font><input TYPE="text" NAME="nom">
+<br><font face="Arial,Helvetica"><font size=+1>&nbsp;Adresse e-m@il :</font></font><input TYPE="text" NAME="email">
+<br><font face="Arial,Helvetica"><font size=+1>&nbsp; Page web perso :</font></font><input TYPE="text" NAME="pageweb">
+<p><font face="Arial,Helvetica"><font size=+1>Vos commentaires :</font></font>
+<br><textarea name="comment" cols="60" rows="10" wrap="virtual"></textarea>
+<p><font face="Arial,Helvetica"><font size=+1>----- Merci ! -----</font></font>
+<p><input TYPE="submit" VALUE=" O K ">
+<br>&nbsp;
+<br>&nbsp;
+<br>&nbsp;
+<br>&nbsp;
+<p></form></center>
+</td>
+</tr>
+</table>
+
+
+</div>
+ <div id="footer">
+    Copyright Cyril / Nicolas / Pierre 2011
+  </div>
+</div>
+
+
+	</body>
+</html>
